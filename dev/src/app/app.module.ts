459326e31b320ef97f332173f13c3b3e31a58bb5
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-09-04 00:10:24 -0600

    ⏣ compiled pages and about-us component

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -7,10 +7,26 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { Slider2Component } from './components/slider2/slider2.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { PromoComponent } from './pages/promo/promo.component';
+import { StartComponent } from './pages/start/start.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 
 const appRoutes: Routes = [
   { path: 'HomeComponent', component: HomeComponent },
+  { path: 'AboutComponent', component: AboutComponent },
+  { path: 'BlogComponent', component: BlogComponent },
+  { path: 'PortfolioComponent', component: PortfolioComponent },
+  { path: 'PromoComponent', component: PromoComponent },
+  { path: 'ServicesComponent', component: ServicesComponent },
+  { path: 'StartComponent', component: StartComponent },
+  { path: 'TestimonialsComponent', component: TestimonialsComponent },
   { path: '**', component: HomeComponent }
 ];
 
@@ -20,7 +36,16 @@ const appRoutes: Routes = [
     NavbarComponent,
     SliderComponent,
     Slider2Component,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    PromoComponent,
+    StartComponent,
+    TestimonialsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
